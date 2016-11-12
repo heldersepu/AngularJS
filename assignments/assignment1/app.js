10065f57bd316d2b8dc1111a1ca2473effd437ea
HelderSepu <heldersepu@gmail.com>
2016-11-11 19:06:29 -0500

    Change indentation and add msgClass

--- assignments/assignment1/app.js
+++ assignments/assignment1/app.js
@@ -1,21 +1,22 @@
 (function () {
-  'use strict';
+	'use strict';
 
-  angular.module('test1', [])
-  .controller('ControllerOne', function ($scope) {
-    $scope.message = "";
-    $scope.btnClick = function () {
-
-      $scope.message = "Please enter data first";
-      if (!isEmpty($scope.input))
-      {
-        $scope.message = "Enjoy!";
-        if ($scope.input.split(",").length > 3)
-          $scope.message = "Too much!";
-      }
-    };
-
-  })
+	angular.module('LunchCheckController', [])
+	.controller('ControllerOne', ['$scope', function ($scope) {
+		$scope.message = "";
+		$scope.msgClass = "";
+		$scope.btnClick = function () {
+			$scope.message = "Please enter data first";
+			$scope.msgClass = "text-danger";
+			if (!isEmpty($scope.input))
+			{
+				$scope.msgClass = "text-success";
+				$scope.message = "Enjoy!";
+				if ($scope.input.split(",").length > 3)
+					$scope.message = "Too much!";
+			}
+		};
+	}])
 })();
 
 function isEmpty(val)
